30bac7be54e3888bdab22128d1b0274a83ac429a
Talha Adıgüzel <talhadiguzel@gmail.com>
2024-04-20 18:01:38 +0300

    mail kısmen eklendi

--- mail.js
+++ mail.js
@@ -11,57 +11,54 @@ require('dotenv').config();
 const period = process.env.period;
 
 // Haftalık yedekleme işlemi
-const weeklycheck = (req, res) => {
-      // Tüm öğrenci kayıtlarını getiren sorgu
+const sendMail = (mailOptions) => {
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'talhadiguzel@gmail.com',
+          pass: process.env.gmail_password
+      }
+  });
+
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          console.log('Error occurred while sending email:', error);
+      } else {
+          console.log('Email sent:', info.response);
+      }
+  });
+};
+
+const weeklycheck = () => {
+  // Tüm öğrenci kayıtlarını getiren sorgu
   const query = "SELECT * FROM ogrenci";
   pool.query(query, (err, result) => {
-    if (err) {
-      res.status(500).send(err.message);
-    } else {
-      if (result.rows.length === 0){
-        res.status(500).send({message: "Ogrenci kaydi bulunamadi."})
-      }else{
-        res.status(200).send({ message: 'Öğrenci bilgileri başarıyla cekildi', result: result.rows});
-        fs.writeFileSync('ogrenciList.json', JSON.stringify(result.rows));
+      if (err) {
+          console.error("Öğrenci bilgileri çekilirken bir hata oluştu:", err);
+      } else {
+          if (result.rows.length === 0) {
+              console.error("Ogrenci kaydi bulunamadi.");
+          } else {
+              console.log('Öğrenci bilgileri başarıyla çekildi');
+              fs.writeFileSync('ogrenciList.json', JSON.stringify(result.rows));
+              const mailOptions = {
+                  from: 'talhadiguzel@gmail.com',
+                  to: 'furkankazimc@gmail.com',
+                  subject: 'Weekly Student List Backup',
+                  text: 'Attached is the weekly backup of student list.',
+                  attachments: [{
+                      filename: 'ogrenciList.json',
+                      path: './ogrenciList.json'
+                  }]
+              };
+              sendMail(mailOptions);
+          }
       }
-    }
-  })
-
-    
-
-    // E-posta gönderme işlemi
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'talhadiguzel@gmail.com',
-            pass: process.env.gmail_password
-        }
-    });
-
-    const mailOptions = {
-        from: 'talhadiguzel@gmail.com',
-        to: 'talhadiguzel@gmail.com',
-        subject: 'Weekly Student List Backup',
-        text: 'Attached is the weekly backup of student list.',
-        attachments: [{
-            filename: 'ogrenciList.json',
-            path: './ogrenciList.json'
-        }]
-    };
-
-    sendMail(mailOptions)
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('Error occurred while sending email:', error);
-        } else {
-            console.log('Email sent:', info.response);
-        }
-    });
+  });
 };
 
 // Haftalık yedekleme işlemini ve e-posta gönderimini periyodik olarak çalıştır
-cron.schedule(`0 0 * * ${period}`, () => {
-    console.log('Running weekly backup...');
-    weeklycheck();
+cron.schedule(`*/1 * * * *`, () => {
+  console.log('Running weekly backup...');
+  weeklycheck();
 });
